883aa0cfeb8f853b83c16b9124148f0af2eae227
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-14 13:53:50 +0700

    init

--- src/queries/authQuery.js
+++ src/queries/authQuery.js
@@ -2,26 +2,6 @@ const db = require("../models");
 const { Op } = require("sequelize");
 const users = db.users;
 
-const registerQuery = async (fullname, address, email, username, password, role_id) => {
-  try {
-    const res = await users.create(
-      {
-        fullname,
-        address,
-        email,
-        username,
-        password,
-        role_id,
-        status: true
-      },
-    );
-    
-    return res;
-  } catch (err) {
-    throw err;
-  }
-};
-
 const loginQuery = async (username) => {
   try {
     const res = await users.findOne({
@@ -51,7 +31,6 @@ const keepLoginQuery = async (id) => {
 };
 
 module.exports = {
-  registerQuery,
   loginQuery,
   keepLoginQuery,
 };
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const { verifyToken } = require("../middleware/auth");
 const { uploadAvatarFile } = require("../middleware/multer");
 const {
-  registerController,
   loginController,
   keepLoginController,
 } = require("../controller/authController");
 
 router.post("/login", loginController);
-router.post("/register", registerController);
 router.get("/keep-login", verifyToken, keepLoginController);
 
 module.exports = router;
