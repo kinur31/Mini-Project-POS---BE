29985c07968206e1fc0a15fafad2fd914556c123
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-15 23:16:02 +0700

    init

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const { verifyToken } = require("../middleware/auth");
-const { uploadAvatarFile } = require("../middleware/multer");
 const {
   registerController,
   loginController,
