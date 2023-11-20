cff8b2ff6205fec9a4aa8958c5e23b28247f45b9
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-20 21:21:32 +0700

    init

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,17 +1,17 @@
-// const { validationResult } =  require("express-validator");
+const { validationResult } = require("express-validator");
 
-// const validator = ( validations ) => {
-//     return async (req, res, next) => {
-//         for (let validation of validations) {
-//             const result = await validation.run(req);
-//             if (result.errors.length) break;
-//         }
-//         const errors = validationResult(req);
-//         if (errors.isEmpty()) {
-//             return next();
-//         }
-//         return res.status(400).json({ errors: errors.array() });
-//     }
-// };
+const validator = (validations) => {
+  return async (req, res, next) => {
+    for (let validation of validations) {
+      const result = await validation.run(req);
+      if (result.errors.length) break;
+    }
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+    return res.status(400).json({ errors: errors.array() });
+  };
+};
 
-// module.exports = validator;
+module.exports = validator;
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,6 +1,8 @@
+import { body } from "express-validator";
 const express = require("express");
 const router = express.Router();
 
+const { validator } = require("../middleware/validator");
 const { verifyToken } = require("../middleware/auth");
 const {
   forgotPasswordController,
@@ -9,8 +11,22 @@ const {
   resetPasswordController,
 } = require("../controller/authController");
 
-router.patch("/forgot-password", forgotPasswordController);
-router.post("/login", loginController);
+const validations = [
+  body("email").notEmpty().withMessage("Email cannot be emptied"),
+  body("email").isEmail().withMessage("Email format is invalid"),
+  body("password").notEmpty().withMessage("Password cannot be emptied"),
+];
+const emailValid = [
+  body("email").notEmpty().withMessage("Email cannot be emptied"),
+  body("email").isEmail().withMessage("Email format is invalid"),
+];
+
+router.patch(
+  "/forgot-password",
+  validator(emailValid),
+  forgotPasswordController
+);
+router.post("/login", validator(validations), loginController);
 router.get("/keep-login", verifyToken, keepLoginController);
 router.patch("/reset-password", resetPasswordController);
 
