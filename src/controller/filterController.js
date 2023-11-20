4ebedaf680a78e987f93f4bb092d64a35b1e9833
Ryan Praka Adikawa <142961699+rypdw@users.noreply.github.com>
2023-11-20 12:41:36 +0700

    init

--- src/controller/filterController.js
+++ src/controller/filterController.js
@@ -1,35 +1,29 @@
-const { filterProductService, paginationProductService } = require("../services/filterService");
-
-const filterProductController = async (req, res) => {
+const {
+    filterProductService,
+  } = require("../services/filterService");
+  
+  const filterProductController = async (req, res) => {
     try {
-        const { productName, productCategory, page, pageSize } = req.query;
-        const result = await filterProductService( productName, productCategory, page, pageSize );
-
-        return res.status(200).json({
-            message: "Success",
-            data: result.product,
-            totalPage: result.totalPage
-        })
+      const { productName, productCategory, sortBy, page, pageSize } = req.query;
+      const result = await filterProductService(
+        productName,
+        productCategory,
+        sortBy,
+        page,
+        pageSize
+      );
+  
+      return res.status(200).json({
+        message: "Success",
+        data: result.product,
+        totalPage: result.totalPage,
+      });
     } catch (err) {
-        return res.status(500).send(err?.message);
+      return res.status(500).send(err?.message);
     }
-}
-
-const paginationProductController = async (req, res) => {
-    try {
-        const {page, pageSize} = req.query;
-
-        const result = await paginationProductService(page, pageSize);
-        return res.status(200).json({
-          message: "Success",
-          data: result
-        })
-      } catch (err) {
-        throw err;
-      }
-}
-
-module.exports = {
+  };
+  
+  module.exports = {
     filterProductController,
-    paginationProductController
-}
+  };
+  
