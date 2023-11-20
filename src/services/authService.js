24fcfc2d15b55e14aeb20398007597543c5f7558
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-20 22:59:04 +0700

    init

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,6 +5,67 @@ const { registerQuery, keepLoginQuery } = require("../queries/authQuery");
 const { findUserQuery } = require("../queries/userQuery");
 const transporter = require("../utils/nodemailer");
 
+const forgotPasswordService = async (email) => {
+  try {
+    const check = await findUserQuery({ email });
+    if (!check) throw new Error("Email doesnt exist");
+
+    const temp = await fs.readFileSync(
+      path.join(__dirname, "../template", "registration-template.html"),
+      "utf-8"
+    );
+
+    let payload = {
+      id: check.id,
+      email: check.email,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1hr",
+    });
+
+    await forgotPasswordQuery(email, token);
+
+    const resetPasswordLink = `${process.env.FE_BASE_URL}/auth/reset-password?token=${token}`;
+    console.log(resetPasswordLink);
+
+    const tempCompile = await handlebars.compile(temp);
+    const tempResult = tempCompile({
+      email: check.email,
+      link: resetPasswordLink,
+    });
+
+    await transporter.sendMail({
+      from: process.env.GMAIL_USER,
+      to: check.email,
+      subject: "Activation",
+      html: tempResult,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const resetPasswordService = async (token, password) => {
+  try {
+    const secretKey = process.env.JWT_SECRET_KEY;
+    if (!secretKey) {
+      throw new Error("JWT_SECRET_KEY is not set in the environment");
+    }
+
+    const decoded = jwt.verify(token, secretKey);
+    if (typeof decoded === "object" && "email" in decoded) {
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(password, salt);
+      await resetPasswordQuery(decoded.email, hashPassword);
+    } else {
+      throw new Error("Invalid token");
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
 const registerService = async (
   fullname,
   address,
@@ -82,6 +143,8 @@ const keepLoginService = async (id) => {
 };
 
 module.exports = {
+  forgotPasswordService,
+  resetPasswordService,
   registerService,
   loginService,
   keepLoginService,
