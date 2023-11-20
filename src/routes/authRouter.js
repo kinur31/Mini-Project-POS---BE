4a26041955e6e4f50953e0b1a96e50e9d3e84656
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-20 22:51:58 +0700

    init

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 
+const { validator } = require("../middleware/validator");
 const { verifyToken } = require("../middleware/auth");
 const {
-  registerController,
+  forgotPasswordController,
   loginController,
   keepLoginController,
+  resetPasswordController,
 } = require("../controller/authController");
 
-router.post("/register", registerController);
-router.post("/login", loginController);
+const validations = [
+  body("username")
+    .notEmpty()
+    .withMessage("Username cannot be empty")
+    .withMessage("Invalid username format"),
+  body("password").notEmpty().withMessage("Password cannot be empty"),
+];
+
+const emailValidations = [
+  body("email")
+    .notEmpty()
+    .withMessage("Email cannot be empty")
+    .isEmail()
+    .withMessage("Invalid email format"),
+];
+
+router.patch(
+  "/forgot-password",
+  validator(emailValidations),
+  forgotPasswordController
+);
+router.post("/login", validator(validations), loginController);
 router.get("/keep-login", verifyToken, keepLoginController);
+router.patch("/reset-password", resetPasswordController);
 
 module.exports = router;
