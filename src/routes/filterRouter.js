50096b03c9ebe9bdb3328a2fcb39fb8b27a0ee05
Ryan Praka Adikawa <142961699+rypdw@users.noreply.github.com>
2023-11-20 12:55:43 +0700

    update-filter

--- src/routes/filterRouter.js
+++ src/routes/filterRouter.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-const {
-  filterProductController,
-} = require("../controllers/filterController");
+const { filterProductController } = require("../controller/filterController");
 
 router.get("/", filterProductController);
 router.get("/search", filterProductController);
